616a7b29627e4bcc27f3a57957a237f432993088
Fabrice Karenzi <kafabkaren@gmail.com>
2025-01-19 16:50:05 +0100

    Applied prettier settings and montserrat font

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,11 @@
-
 function App() {
-  
-
   return (
     <>
-      <h1 className="text-3xl text-teal-600 font-semibold font-Montserrat">
+      <h1 className="text-bold font-Montserrat text-2xl text-teal-600">
         Champion Quiz
       </h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
