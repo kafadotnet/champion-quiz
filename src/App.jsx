dbc9074287c067c42e4e27e6c8cd382dfe0da382
Fabrice Karenzi <kafabkaren@gmail.com>
2025-01-19 02:54:10 +0100

    Init commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+function App() {
+  
+
+  return (
+    <>
+      <h1 className="text-3xl text-teal-600 font-semibold font-Montserrat">
+        Champion Quiz
+      </h1>
+    </>
+  )
+}
+
+export default App
