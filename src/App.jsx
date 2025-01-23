408d2b9b67feb57ba7fa06df760761b0c5781f74
Fabrice Karenzi <kafabkaren@gmail.com>
2025-01-23 16:15:34 +0100

    Added Get Started page

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import PageNotFound from './pages/PageNotFound';
+import GetStarted from './pages/GetStarted';
 
 function App() {
   return (
       <>
         <BrowserRouter>
           <Routes>
-            <Route path='/' element={<HomePage />} />
+            <Route path='/' element={<GetStarted />} />
+            <Route path='/home-page' element={<HomePage />} />
             <Route path='*' element={<PageNotFound />} />
           </Routes>
         </BrowserRouter>
---
+++ src/pages/GetStarted.jsx
@@ -0,0 +1,49 @@
+import { NavLink } from "react-router-dom";
+
+const GetStarted = () => {
+  return (
+    <section className="h-screen bg-gray-700 font-Montserrat text-white">
+      <section id="navigation" className="h-screen bg-gray-700 text-white">
+        <nav className="flex justify-between px-8 py-8">
+          <div>
+            <h1>
+              <NavLink to="/" className="p-5 text-3xl font-semibold">
+                Champion Quiz
+              </NavLink>
+            </h1>
+          </div>
+          <ul className="flex">
+            <li>
+              <NavLink
+                to="/instructions"
+                className="btn btn:hover p-5 text-lg font-semibold"
+              >
+                Instructions
+              </NavLink>
+            </li>
+          </ul>
+        </nav>
+
+        <main className="px-16 py-16">
+          <header className="mt-12 pb-8 text-center">
+            <h3 className="text-3xl font-semibold">Quiz For Champions</h3>
+            <p className="pt-4 text-base text-white">
+              Click Get Started to test your General Knowledge arsenal!
+            </p>
+          </header>
+
+          <NavLink to="/home-page" className="">
+            <div className="flex items-center justify-center pt-20">
+              <div className="btn btn:hover relative h-40 w-40 rounded-full bg-green-500"></div>
+              <div className="btn-start absolute h-10 w-60 bg-gray-100 p-2 opacity-75">
+                <h4 className="btn-start-font text-center">Get Started</h4>
+              </div>
+            </div>
+          </NavLink>
+        </main>
+      </section>
+    </section>
+  );
+};
+
+export default GetStarted;
