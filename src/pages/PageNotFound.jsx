306302cb11d15a5c206da486ccf27a31a365c49e
Fabrice Karenzi <kafabkaren@gmail.com>
2025-01-24 08:34:27 +0100

    Added button hover transition

--- src/pages/PageNotFound.jsx
+++ src/pages/PageNotFound.jsx
@@ -2,7 +2,7 @@ import { NavLink } from "react-router-dom";
 
 const PageNotFound = () => {
   return (
-    <section className="h-screen bg-gray-700 font-Montserrat p-4">
+    <section className="h-screen bg-gray-700 p-4 font-Montserrat">
       <nav className="flex justify-between p-8">
         <div>
           <h1>
@@ -15,37 +15,50 @@ const PageNotFound = () => {
           <li>
             <NavLink
               to="/instructions"
-              className="btn btn:hover p-5 text-lg font-semibold"
+              className="btn btn:hover p-5 text-lg font-semibold transition duration-500 ease-out"
             >
               Instructions
             </NavLink>
           </li>
         </ul>
       </nav>
-      <main className="p-5 text-center ml-7">
+      <main className="ml-7 p-5 text-center">
         <div className="About-quiz text-white">
           <h3 className="pt-10 text-center font-Montserrat text-xl font-semibold text-white">
             About the general knowledge quiz
           </h3>
-          <p className="text-white pt-4">You can test your General knowledge skills with Champion' Quiz.</p>
+          <p className="pt-4 text-white">
+            You can test your General knowledge skills with Champion' Quiz.
+          </p>
         </div>
         <div className="About-quiz text-white">
           <h3 className="pt-6 text-center font-Montserrat text-xl font-semibold text-white">
             The quiz
           </h3>
-          <p className="text-white pt-4">The quiz contains 10 questions and these are time bound through timer (10 sec. Max).</p>
+          <p className="pt-4 text-white">
+            The quiz contains 10 questions and these are time bound through
+            timer (10 sec. Max).
+          </p>
         </div>
         <div className="About-quiz text-white">
           <h3 className="pt-6 text-center font-Montserrat text-xl font-semibold text-white">
             Count Your Score
           </h3>
-          <p className="text-white pt-4">You will get 1 point for each correct answer. At the end of the Quiz, your total score will be displayed. Maximum score is 10 points.</p>
+          <p className="pt-4 text-white">
+            You will get 1 point for each correct answer. At the end of the
+            Quiz, your total score will be displayed. Maximum score is 10
+            points.
+          </p>
         </div>
         <div className="flex items-center justify-center pt-20">
-          <NavLink to='/home-page' className='btn'>Start Playing <i className="fa-solid fa-play"></i></NavLink>
+          <NavLink
+            to="/home-page"
+            className="btn transition duration-500 ease-out"
+          >
+            Start Playing <i className="fa-solid fa-play"></i>
+          </NavLink>
         </div>
       </main>
-      
     </section>
   );
 };
